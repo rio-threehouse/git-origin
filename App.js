219f5233d56328c586ba3a2c9fa62ae8c3cda6e7
Rio Sanka <threehouse.second@gmail.com>
2019-11-29 13:01:01 +0900

    master5

--- App.js
+++ App.js
@@ -15,7 +15,6 @@ const styles = StyleSheet.create({
   container: {
     flex: 1,
     backgroundColor: '#fff',
-    backgroundColor: '#fff',
     alignItems: 'center',
     justifyContent: 'center',
   },
